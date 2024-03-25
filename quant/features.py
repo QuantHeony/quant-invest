d649f0370d6330221436f4c80335449e1ea44fb0
jaeheon <jaeheon.choi@varodrt.com>
2024-03-26 01:13:03 +0900

    * 한투 계좌 연결 * 정적자산-자동 리벨랜싱 * 매수매도 구현

---
+++ quant/features.py
@@ -0,0 +1,204 @@
+import pykis
+from pykis import *
+from typing import Dict
+from collections import defaultdict
+from prettytable import PrettyTable
+import copy
+import numpy as np
+
+
+def adjustRebalancing(kis:PyKis, balance:KisAccountBalance, weightDict:Dict[str, Dict], bullet=None):
+    # rebalancing ratio가 1인지 체크
+    totalRatio = 0
+    n = 0
+    for ticker in weightDict:
+        totalRatio += weightDict[ticker]["ratio"]
+        if weightDict[ticker]["name"] != kis.stock(str(ticker)).name:
+            raise Exception(f"[ERROR] 이름 오류 {weightDict[ticker]['name']} != {kis.stock(str(ticker)).name}")
+        n+=1
+    if totalRatio != 1 :
+        raise Exception(f"[ERROR] 비율의 합 1 != {totalRatio}")
+
+    priceInfo = np.zeros(n) # 가격 리스트
+    ratioList = np.zeros(n) # 비율 계산 리스트
+    countList = np.zeros(n) # 구매 개수 리스트
+    totalPriceList = np.zeros(n) # 소모 금액
+    diffCountList = np.zeros(n) # 개수를 정수화 하면서 잃은 비율
+    tickerList = sorted(list(weightDict.keys())) # ticker 리스트
+    tickerIndexDict = defaultdict(str) # key : ticker -> val: tickerList index
+    for idx, ticker in enumerate(tickerList):
+        tickerIndexDict[ticker] = idx
+
+    # 정해진 구매액이 없다면 예수금을 전부 다 사용
+    if not bullet:
+        deposit = balance.tot_evlu_amt # 평가금
+    else :
+        deposit = bullet
+
+    totalResidual = 0
+    for i,ticker in enumerate(tickerList):
+        stock = kis.stock(str(ticker))
+        price = stock.price().stck_prpr
+        count = int(deposit * weightDict[ticker]["ratio"] / price)
+        diffCountList[i] = (deposit * weightDict[ticker]["ratio"] / price) - count
+        priceInfo[i] = price
+
+        countList[i] = count
+        ratioList[i] = (count * price) / deposit
+        totalPriceList[i] = count * price
+
+        totalResidual += abs(ratioList[i] - weightDict[ticker]["ratio"])
+
+    bestCountList = copy.deepcopy(countList)
+    bestRatioList = copy.deepcopy(ratioList)
+    bestTotalPriceList = copy.deepcopy(totalPriceList)
+    bestDiff = deposit - sum(totalPriceList)
+    bestTotalResidual = totalResidual
+    bestDiffCountList = diffCountList
+
+    while True:
+        toUpTickerIdx = np.argmax(bestDiffCountList)
+        tempDiff = priceInfo[toUpTickerIdx]
+        if bestDiff > tempDiff :
+            tempCountList = copy.deepcopy(bestCountList)
+            tempCountList[toUpTickerIdx] += 1
+            tempTotalPriceList = copy.deepcopy(totalPriceList)
+            tempTotalPriceList[toUpTickerIdx] += priceInfo[toUpTickerIdx]
+
+            tempSumPrice = sum(tempTotalPriceList)
+            tempRatioList = np.zeros(n)
+            tempTotalResidual = 0
+            for idx, ticker in enumerate(tickerList):
+                tempRatioList[idx] = tempTotalPriceList[idx]/tempSumPrice
+                tempTotalResidual += abs(tempRatioList[i] - weightDict[ticker]["ratio"])
+            if bestTotalResidual > tempTotalResidual:
+                bestCountList = copy.deepcopy(tempCountList)
+                bestRatioList = tempRatioList
+                bestDiff = bestDiff - tempDiff
+                bestTotalResidual = tempTotalResidual
+                bestTotalPriceList = tempTotalPriceList
+                bestDiffCountList[toUpTickerIdx] += 1
+            else:
+                break
+        else:
+            break
+    print("\n" + "=" * 30 + " Rebalancing Result " + "=" * 30 + "\n")
+
+    table = PrettyTable(field_names=[
+        '상품번호',
+        '상품명',
+        '리밸런싱 수량',
+        '매입금액',
+        '현재가',
+        '목표 비율',
+        '리밸런싱 비율',
+        "비율 차이"
+    ],
+        align='r',
+    )
+
+    for idx, ticker in enumerate(tickerList):
+        table.add_row([
+            ticker,
+            weightDict[ticker]["name"],
+            f'{int(bestCountList[idx]):,}주',
+            f'{int(bestTotalPriceList[idx]):,}원',
+            f'{int(priceInfo[idx]):,}원',
+            f'{weightDict[ticker]["ratio"] * 100:.2f}%',
+            f'{bestRatioList[idx] * 100:.2f}%',
+            f'{(bestRatioList[idx] - weightDict[ticker]["ratio"]) * 100:.2f}%'
+        ])
+    print(table)
+
+
+    orderList = []
+    taken = []
+    for stock in balance.stocks:
+        ticker = stock.pdno
+        idx = tickerIndexDict[ticker]
+
+        doc = {
+            "ticker" : ticker,
+            "order" : int(bestCountList[idx] - stock.hldg_qty),
+            "price" : int(priceInfo[idx])
+        }
+        taken.append(ticker)
+        orderList.append(doc)
+
+        if doc['order'] > 0:
+            print(f"* 추가 매수 필요 수량: [{ticker} | {weightDict[ticker]['name']}] \t (+{doc['order']}개) 금액: {int(priceInfo[idx] * doc['order']):,}원")
+        else :
+            print(f"* 매도 필요 수량: [{ticker} | {weightDict[ticker]['name']}] \t ({doc['order']}개) 금액: -{int(priceInfo[idx] * doc['order']):,}원")
+
+    for ticker in tickerList:
+        if ticker not in taken:
+            idx = tickerIndexDict[ticker]
+            doc = {
+                "ticker" : ticker,
+                "order" : int(bestCountList[idx]),
+                "price" : int(priceInfo[idx])
+            }
+            orderList.append(doc)
+            taken.append(ticker)
+            if doc['order'] > 0:
+                print(f"* 추가 매수 필요 수량: [{ticker} | {weightDict[ticker]['name']}] \t (+{doc['order']}개) 금액: {int(priceInfo[idx] * doc['order']):,}원")
+    print("")
+    print(f"* 구매시 예수금 변화 : {balance.dnca_tot_amt:,}원 -> {int(bestDiff):,}원 ")
+    return orderList
+
+
+'''
+매수 
+'''
+def orderStock(account:pykis.KisAccountScope, ticker, count, orderPrice=0):
+    print(f"*매수 {ticker}, qty={count}, orderPrice={orderPrice} (0일경우 시장가)")
+    if orderPrice == 0: # 시장가
+        return account.buy(ticker, qty=count, unpr=0, dvsn='시장가')
+    else :
+        return account.buy(ticker, qty=count, unpr=orderPrice)
+'''
+매도
+'''
+def sellStock(account:pykis.KisAccountScope, ticker, count, sellPrice=0):
+    print(f"*매도 {ticker}, qty={count}, sellPrice={sellPrice} (0일경우 시장가)")
+    if sellPrice == 0: # 시장가
+        return account.sell(ticker, qty=count, unpr=0, dvsn='시장가')
+    else :
+        return account.buy(ticker, qty=count, unpr=sellPrice)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ quant/korea_all_weather.py
@@ -0,0 +1,102 @@
+from prettytable import PrettyTable
+from pykis import *
+from features import *
+
+with open("../hts-isa.txt", "r") as f:
+    for line in f:
+        key, value = line.strip().split(':')
+
+        if key == 'APPKEY':
+            APP_KEY = value
+        elif key == 'APPSECRET':
+            APP_SECRET = value
+        elif key == 'ACCOUNTNO':
+            ACCOUNT_NO = value
+
+
+# 11 To 4
+KoreanAllWeather11To4 = {
+    '360750' : {
+        "name" : "TIGER 미국S&P500",
+        "ratio" : 0.2
+    },
+    '294400' : {
+        "name" : "KOSEF 200TR",
+        "ratio" : 0.2
+    },
+    '132030' : {
+        "name" : "KODEX 골드선물(H)",
+        "ratio" : 0.15
+    },
+    '148070' :  {
+        "name" : "KOSEF 국고채10년",
+        "ratio" : 0.225
+    },
+    '305080' : {
+        "name" : "TIGER 미국채10년선물",
+        "ratio" : 0.225
+    }
+}
+
+if __name__ == "__main__":
+    kis = PyKis(
+        appkey=APP_KEY,
+        appsecret=APP_SECRET,
+        virtual_account=False, # 가상 계좌 여부
+        realtime=True # 실시간 조회 비활성화
+    )
+    # 계좌 스코프를 생성한다.
+    account = kis.account(ACCOUNT_NO)
+    # 계좌 잔고를 조회한다.
+    balance = account.balance_all()
+    table = PrettyTable(field_names=[
+        '상품번호',
+        '상품명',
+        '보유수량',
+        '매입금액',
+        '현재가',
+        '평가손익율',
+        '평가손익',
+    ],
+        align='r',
+    )
+
+    print(f"\n [현재 잔고 조회]")
+
+    print(f'예수금: {balance.dnca_tot_amt:,}원 평가금: {balance.tot_evlu_amt:,} 손익: {balance.evlu_pfls_smtl_amt:,}원')
+
+    for stock in balance.stocks:
+        table.add_row([
+            stock.pdno,
+            stock.prdt_name,
+            f'{stock.hldg_qty:,}주',
+            f'{stock.pchs_amt:,}원',
+            f'{stock.prpr:,}원',
+            f'{stock.evlu_pfls_rt:.2f}%',
+            f'{stock.evlu_pfls_amt:,}원',
+        ])
+
+    print(table)
+
+    # 리밸런싱 수량 확인
+    orderList = adjustRebalancing(kis, balance, KoreanAllWeather11To4)
+
+
+    ORDER_FLAG = False
+
+    if ORDER_FLAG:
+        # 주문 or 매수
+        for doc in orderList:
+            ticker = doc["ticker"]
+            cnt = doc["order"]
+            price = doc["price"]
+            if cnt > 0:
+                orderStock(account,ticker, cnt, price)
+            else:
+                sellStock(account,ticker, cnt, price)
+
+
+
+    # FM 대로 사용 다한 임시 토큰은 삭제함.
+    if kis.client.token:
+        kis.client.token.discard()
