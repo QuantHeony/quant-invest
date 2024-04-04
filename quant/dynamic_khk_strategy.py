749cb60bf4ba856aa5a51fa53341e23f66ac0369
QuantHeony <150584909+QuantHeony@users.noreply.github.com>
2024-04-05 02:22:25 +0900

    * 변형 듀얼 모멘텀 완성

--- quant/dynamic_khk_strategy.py
+++ quant/dynamic_khk_strategy.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime, timedelta
 from custom_logger import CustomLogger
 from strategy.vaa import Vaa
-from strategy.dual_momentum import DualMomentum
+from strategy.modified_dual_momentum import ModifiedDualMomentum
 from strategy.laa import Laa
 from prettytable import PrettyTable
 from pykis import *
@@ -21,11 +21,11 @@ with open("../hts.txt", "r") as f:
             ACCOUNT_NO = value
 
 '''
-khk strategy
+khk strategy -> 변경 
 
 1. VAA 공격형 - 33.4%
 2. LAA - 33.3%
-3. 오리지날 듀얼 모멘텀 - 33.3%
+3. 변형 듀얼 모멘텀 - 33.3%
 '''
 
 khkStrategy = {
@@ -37,7 +37,7 @@ khkStrategy = {
         "target" : None,
         "ratio" : 0.333
     },
-    'DUAL' : {
+    'MODIFIED_DUAL' : {
         "target" : None,
         "ratio" : 0.333
     }
@@ -53,20 +53,26 @@ if __name__ == "__main__":
     todayStr = today.strftime("%Y-%m-%d")
     startDateStr = startDate.strftime("%Y-%m-%d")
 
-    # VOO
-    vaa = Vaa(logger, startDateStr, todayStr)
-    vaa.getVAAWeight()
-    khkStrategy["VAA"]["target"] = vaa.target
-
-    # LAA
-    laa = Laa(logger, FRED_API_KEY, todayStr)
-    laa.pickTargets()
-    khkStrategy["LAA"]["target"] = laa.target
+    # # VOO
+    # vaa = Vaa(logger, startDateStr, todayStr)
+    # vaa.getVAAWeight()
+    # khkStrategy["VAA"]["target"] = vaa.target
+    #
+    # # LAA
+    # laa = Laa(logger, FRED_API_KEY, todayStr)
+    # laa.pickTargets()
+    # khkStrategy["LAA"]["target"] = laa.target
 
     # Dual Momentum
-    dualM = DualMomentum(logger, todayStr)
-    dualM.pickTarget()
-    khkStrategy["DUAL"]["target"] = dualM.target
+    # dualM = DualMomentum(logger, todayStr)
+    # dualM.pickTarget()
+    # khkStrategy["DUAL"]["target"] = dualM.target
+
+    # Modified Dual Momentum
+    modifiedDualM = ModifiedDualMomentum(logger, todayStr)
+    modifiedDualM.pickTarget()
+    khkStrategy["MODIFIED_DUAL"]["target"] = modifiedDualM.target
+
 
     # TODO. 주문시 체크!
     ORDER_FLAG = False
@@ -104,44 +110,44 @@ if __name__ == "__main__":
         logger.info(f"* {ticker} \t" + "-" * 20 + f"{PORTFOLIO_DICT[ticker]['ratio'] * 100} %")
 
 
-    kis = PyKis(
-        appkey=APP_KEY,
-        appsecret=APP_SECRET,
-        virtual_account=False, # 가상 계좌 여부
-        realtime=True # 실시간 조회 비활성화
-    )
-
-    # 계좌 스코프를 생성한다.
-    account = kis.account(ACCOUNT_NO)
-    # 계좌 잔고를 조회한다.
-    balance = account.balance_all()
-    table = PrettyTable(field_names=[
-        '상품번호',
-        '상품명',
-        '보유수량',
-        '매입금액',
-        '현재가',
-        '평가손익율',
-        '평가손익',
-    ],
-        align='r',
-    )
-
-    print(f"\n [현재 잔고 조회]")
-    print(f'예수금: {balance.dnca_tot_amt:,}원 평가금: {balance.tot_evlu_amt:,} 손익: {balance.evlu_pfls_smtl_amt:,}원')
-
-    for stock in balance.stocks:
-        table.add_row([
-            stock.pdno,
-            stock.prdt_name,
-            f'{stock.hldg_qty:,}주',
-            f'{stock.pchs_amt:,}원',
-            f'{stock.prpr:,}원',
-            f'{stock.evlu_pfls_rt:.2f}%',
-            f'{stock.evlu_pfls_amt:,}원',
-        ])
-
-    print(table)
+    # kis = PyKis(
+    #     appkey=APP_KEY,
+    #     appsecret=APP_SECRET,
+    #     virtual_account=False, # 가상 계좌 여부
+    #     realtime=True # 실시간 조회 비활성화
+    # )
+    #
+    # # 계좌 스코프를 생성한다.
+    # account = kis.account(ACCOUNT_NO)
+    # # 계좌 잔고를 조회한다.
+    # balance = account.balance_all()
+    # table = PrettyTable(field_names=[
+    #     '상품번호',
+    #     '상품명',
+    #     '보유수량',
+    #     '매입금액',
+    #     '현재가',
+    #     '평가손익율',
+    #     '평가손익',
+    # ],
+    #     align='r',
+    # )
+    #
+    # print(f"\n [현재 잔고 조회]")
+    # print(f'예수금: {balance.dnca_tot_amt:,}원 평가금: {balance.tot_evlu_amt:,} 손익: {balance.evlu_pfls_smtl_amt:,}원')
+    #
+    # for stock in balance.stocks:
+    #     table.add_row([
+    #         stock.pdno,
+    #         stock.prdt_name,
+    #         f'{stock.hldg_qty:,}주',
+    #         f'{stock.pchs_amt:,}원',
+    #         f'{stock.prpr:,}원',
+    #         f'{stock.evlu_pfls_rt:.2f}%',
+    #         f'{stock.evlu_pfls_amt:,}원',
+    #     ])
+    #
+    # print(table)
     #
     # # 리밸런싱 수량 확인
     # orderList = adjustRebalancing(kis, balance, PORTFOLIO_DICT)
--- quant/strategy/modified_dual_momentum.py
+++ quant/strategy/modified_dual_momentum.py
@@ -19,7 +19,8 @@ VOO(미국 주식), BIL(미국 초단기 채권), 선진국 주식(EFA)
 
 class ModifiedDualMomentum:
     def __init__(self, logger:CustomLogger, endDate=None):
-        self.mDualMCumReturn = None
+        self.mDualMDefenseCumReturn = None
+        self.mDualMAttackCumReturn = None
         self.target = None # 선택 ETF
         self.logger = logger
         if not endDate:
@@ -27,49 +28,91 @@ class ModifiedDualMomentum:
             self.endDate = today
         else:
             self.endDate = datetime.strptime(endDate, "%Y-%m-%d")
-        self.startDate = (self.endDate - timedelta(days=365)).strftime("%Y-%m-%d")
-        self.mDualMData:pd.DataFrame = pd.DataFrame()
-        self.mDualAttack = ['VOO', "EFA"]
-        self.mDualDefense = ["SHY", "IEF", "TLT", "TIP", "LQD", "HYG","BWX", "EMB"]
-        self.mDualMCol = self.mDualAttack + self.mDualDefense
+        self.aYearAgo = (self.endDate - timedelta(days=365)).strftime("%Y-%m-%d")
+        self.sixMonthAgo = (self.endDate - timedelta(days=180)).strftime("%Y-%m-%d")
+        self.mDualMDataAttack:pd.DataFrame = pd.DataFrame()
+        self.mDualMDataDefense:pd.DataFrame = pd.DataFrame()
+        self.mDualAttackCol = ['VOO', "EFA"]
+        self.mDualDefenseCol = ["SHY", "IEF", "TLT", "TIP", "LQD", "HYG", "BWX", "EMB"]
+        self.mDualMCol = self.mDualAttackCol + self.mDualDefenseCol
 
         self.loadDataSet()
 
-
     def loadDataSet(self):
-        VOO = getCloseData("VOO", self.startDate, self.endDate) # VOO(미국주식)
-        BIL = getCloseData("BIL", self.startDate, self.endDate) # BIL(미국 초단기 채권)
-        EFA = getCloseData("EFA", self.startDate, self.endDate) # EFA(선진국 주식)
-        AGG = getCloseData("AGG", self.startDate, self.endDate) # AGG(미국 총채권)
+        # Attack
+        VOO = getCloseData("VOO", self.aYearAgo, self.endDate) # VOO(미국주식)
+        BIL = getCloseData("BIL", self.aYearAgo, self.endDate) # BIL(미국 초단기 채권)
+        EFA = getCloseData("EFA", self.aYearAgo, self.endDate) # EFA(선진국 주식)
+        # Defense
+        SHY = getCloseData("SHY", self.sixMonthAgo, self.endDate) # SHY(미국 단기국채)
+        IEF = getCloseData("IEF", self.sixMonthAgo, self.endDate) # IEF(미국 중기국채)
+        TLT = getCloseData("TLT", self.sixMonthAgo, self.endDate) # TLT(미국 장기국채)
+        TIP = getCloseData("TIP", self.sixMonthAgo, self.endDate) # TIP(미국 물가연동채)
+        LQD = getCloseData("LQD", self.sixMonthAgo, self.endDate) # LQD(미국 회사채)
+        HYG = getCloseData("HYG", self.sixMonthAgo, self.endDate) # HYG(미국 하이일드)
+        BWX = getCloseData("BWX", self.sixMonthAgo, self.endDate) # BWX(국제 채권)
+        EMB = getCloseData("EMB", self.sixMonthAgo, self.endDate) # EMB(개도국 채권)
 
-        self.mDualMData = pd.concat([VOO, BIL, EFA,  AGG], axis=1)
-        self.mDualMData.columns = self.mDualMCol
+
+        self.mDualMDataAttack = pd.concat([VOO, EFA, BIL], axis=1)
+        self.mDualMDataDefense = pd.concat([SHY, IEF, TLT, TIP, LQD, HYG, BWX, EMB], axis=1)
+        self.mDualMDataAttack.columns = self.mDualAttackCol + ["BIL"]
+        self.mDualMDataDefense.columns = self.mDualDefenseCol
         self.logger.info("=" * 25 + "[Dual Momentum] " + "=" * 25)
-        self.logger.info(f"\n* (Dual Momentum) Completed to load Data set")
+        self.logger.info(f"\n* (Modified Dual Momentum) Completed to load Data set")
 
     # 12 개월 누적수익률
-    def getCumReturn(self) -> pd.DataFrame:
-        rebalDate = getRebalancingDate(self.mDualMData) # 리밸런싱 매월 말 날짜
-        dualMDataOnRebalDate = self.mDualMData.loc[rebalDate] # 매월 말 가격 데이터
+    def getAttackCumReturn(self) -> pd.DataFrame:
+        rebalDate = getRebalancingDate(self.mDualMDataAttack) # 리밸런싱 매월 말 날짜
+        dualMDataOnRebalDate = self.mDualMDataAttack.loc[rebalDate] # 매월 말 가격 데이터
+
+        dualMCumReturn = getCumulativeReturn(dualMDataOnRebalDate)
+        return dualMCumReturn
+
+    def getDefenseCumReturn(self) -> pd.DataFrame:
+        rebalDate = getRebalancingDate(self.mDualMDataDefense) # 리밸런싱 매월 말 날짜
+        dualMDataOnRebalDate = self.mDualMDataDefense.loc[rebalDate] # 매월 말 가격 데이터
 
         dualMCumReturn = getCumulativeReturn(dualMDataOnRebalDate)
         return dualMCumReturn
 
     def pickTarget(self):
-        self.mDualMCumReturn:pd.DataFrame = self.getCumReturn()
-        lastDay = self.mDualMCumReturn.index[-1]
-        self.printWeightAsTable(self.mDualMCumReturn.round(3), f"* (Dual Momentum) 12개월 누적 수익률")
+        self.mDualMAttackCumReturn:pd.DataFrame = self.getAttackCumReturn()
+        lastDay = self.mDualMAttackCumReturn.index[-1]
+        self.printWeightAsTable(self.mDualMAttackCumReturn.round(3), self.mDualMAttackCumReturn.columns.tolist(),f"* (Modified Dual Momentum) Attack 자산 12개월 누적 수익률")
 
-        d = self.mDualMCumReturn.loc[lastDay]
-        if d["VOO"] > d["BIL"]:
-            self.target = [("VOO", 1.0)] if d["VOO"] >= d["EFA"] else [("EFA", 1.0)]
+        attackData = self.mDualMAttackCumReturn.loc[lastDay]
+        if attackData["VOO"] > attackData["BIL"]:
+            self.target = [("VOO", 1.0)] if attackData["VOO"] >= attackData["EFA"] else [("EFA", 1.0)]
         else:
-            self.target = [("AGG", 1.0)]
+            self.mDualMDefenseCumReturn:pd.DataFrame = self.getDefenseCumReturn()
+            lastDay = self.mDualMDefenseCumReturn.index[-1]
+            self.printWeightAsTable(self.mDualMDefenseCumReturn.round(3), self.mDualMDefenseCumReturn.columns.tolist(), f"* (Modified Dual Momentum) Attack 자산 12개월 누적 수익률")
+            defenseData = self.mDualMDefenseCumReturn.loc[lastDay]
+            top3Indices = defenseData.nlargest(3).index.tolist()
 
-        self.logger.info(f'* (Dual Momentum) [{lastDay.strftime("%Y-%m-%d")} 기준] --->> "{self.target}" 선택\n')
+            cash = 0
+            target = []
+            self.target = []
+            for ticker in top3Indices:
+                if defenseData.loc[ticker] > 1 :
+                    ratio = 0.333
+                else :
+                    ratio = 0.0
+                    cash += 1
+                target.append([ticker,ratio])
+            target.append(["cash", cash * 0.333])
+            if cash > 0 :
+                target[-1][1] += 0.001
+            else :
+                target[0][1] += 0.001
+            for ele in target:
+                self.target.append(tuple(ele))
 
-    def printWeightAsTable(self, df, frontText=None):
-        table = PrettyTable(field_names= ["투자 기준일"] + self.mDualMCol,
+        self.logger.info(f'* (Modified Dual Momentum) [{lastDay.strftime("%Y-%m-%d")} 기준] --->> "{self.target}" 선택\n')
+
+    def printWeightAsTable(self, df, col, frontText=None):
+        table = PrettyTable(field_names= ["투자 기준일"] + col,
                             align='r',
                             )
         lastDay = df.index[-1]
